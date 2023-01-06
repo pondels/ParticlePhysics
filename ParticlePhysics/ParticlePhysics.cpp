c84b9f48e93d80f51d9c1519f4e975b2bc812836
pondels <yeckron@comcast.net>
2023-01-05 18:22:30 -0700

    yis

--- ParticlePhysics/ParticlePhysics.cpp
+++ ParticlePhysics/ParticlePhysics.cpp
@@ -14,7 +14,8 @@ void DeltaTime(double& lastTime, double& currentTime, double& deltaTime) {
     deltaTime = (currentTime - lastTime) / 1000.f;
     lastTime = currentTime;
 }
-bool check_collisions(std::vector<Particle*> particles, Particle* particle, sf::CircleShape* shape, int index) {
+
+void check_collisions(std::vector<Particle*> particles, Particle* particle, sf::CircleShape* shape, int index) {
     
     // Update Direction & Speed of particle based on collisions.
 
@@ -40,7 +41,7 @@ bool check_collisions(std::vector<Particle*> particles, Particle* particle, sf::
     }
 
     // Colliding with the walls
-    else if (particlex < radius) {
+    if (particlex < radius) {
         particle->velocity->x = abs(particle->velocity->x) * restitution;
         particle->particle->setPosition(radius, particley);
     }
@@ -55,14 +56,15 @@ bool check_collisions(std::vector<Particle*> particles, Particle* particle, sf::
     for (int i = 0; i < particles.size(); i++) {
         if (i != index) {
             float x1 = particles.at(i)->particle->getPosition().x;
-            float x2 = shape->getPosition().x;
             float y1 = particles.at(i)->particle->getPosition().y;
-            float y2 = shape->getPosition().y;
             float r1 = particles.at(i)->particle->getGlobalBounds().width / 2;
-            float r2 = shape->getGlobalBounds().width / 2;
             double m1 = particles.at(i)->mass;
-            double m2 = particle->mass;
             sf::Vector2f* v1 = particles.at(i)->velocity;
+            
+            float x2 = shape->getPosition().x;
+            float y2 = shape->getPosition().y;
+            float r2 = shape->getGlobalBounds().width / 2;
+            double m2 = particle->mass;
             sf::Vector2f* v2 = particle->velocity;
             
             float squaredistance = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
@@ -75,14 +77,16 @@ bool check_collisions(std::vector<Particle*> particles, Particle* particle, sf::
                 sf::Vector2f vCollisionNorm(vCollision.x / distance, vCollision.y / distance);
                 sf::Vector2f vRelativeVelocity(v1->x - v2->x, v1->y - v2->y);
                 float speed = vRelativeVelocity.x * vCollisionNorm.x + vRelativeVelocity.y * vCollisionNorm.y;
+                if (speed < 0) {
+                    break;
+                }
                 speed *= restitution;
                 float impulse = 2 * speed / (m1 + m2);
 
-                particles.at(index)->velocity->x = impulse * m2 * vCollisionNorm.x;
-                particles.at(index)->velocity->y = impulse * m2 * vCollisionNorm.y;
-                particle->velocity->x = impulse * m1 * vCollisionNorm.x;
-                particle->velocity->y = impulse * m1 * vCollisionNorm.y;
-                break;
+                v1->x -= impulse * m2 * vCollisionNorm.x;
+                v1->y -= impulse * m2 * vCollisionNorm.y;
+                v2->x += impulse * m1 * vCollisionNorm.x;
+                v2->y += impulse * m1 * vCollisionNorm.y;
             }
         }
     }
@@ -90,45 +94,34 @@ bool check_collisions(std::vector<Particle*> particles, Particle* particle, sf::
     if (!overlap) {
         shape->setFillColor(sf::Color(0, 0, 255));
     }
-
-    return true;
 }
+
 void update_position(Particle* particle, sf::CircleShape* shape, int index, double deltaTime) {
     
     // Move particle so far along the given path.
-    float gravity = 9.81 / 5;
+    float gravity = 9.81;
     float x = shape->getPosition().x;
     float y = shape->getPosition().y;
-    shape->setPosition(x + particle->velocity->x, y + particle->velocity->y);
-    
+
     // Applying gravity to particle
-    particle->velocity->y += particle->mass * deltaTime * gravity;
+    particle->velocity->y += deltaTime * gravity * particle->mass;
+
+    shape->setPosition(x + particle->velocity->x * deltaTime, y + particle->velocity->y * deltaTime);
 }
 void update_particle(std::vector<Particle*> particles, int index, double deltaTime) {
 
     check_collisions(particles, particles.at(index), particles.at(index)->particle, index);
     update_position(particles.at(index), particles.at(index)->particle, index, deltaTime);
 }
-
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
  
     std::vector<Particle*> particles;
 
-    int particle_amount = 10;
-
-    for (int i = 0; i < particle_amount; i++) {
-        float size = 15.f;
-        sf::Vector2f position(10 + 45 * i, 50);
-        sf::Color color(0, 0, 255);
-        int grav_type = 1;
-        double mass = .1 + .1*i;
-        sf::Vector2f* velocity = new sf::Vector2f(.1, .1);
-
-        Particle* particle = new Particle(size, position, color, grav_type, mass, velocity);
-        particles.push_back(particle);
-    }
+    // Standard particle features
+    float size = 10.f;
+    double mass = 15.f;
 
     double lastTime = clock();
     double deltaTime;
@@ -141,8 +134,41 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+
+            if (event.type == sf::Event::Closed) window.close();
+            else if (event.type == sf::Event::KeyPressed)
+            {
+                int mouseX = sf::Mouse::getPosition().x;
+                int mouseY = sf::Mouse::getPosition().y;
+
+                if (event.key.code == sf::Keyboard::Add)
+                {
+                    std::cout << particles.size() << std::endl;
+                    size += 1.f;
+                    mass += 5.f;
+                }
+                else if (event.key.code == sf::Keyboard::Subtract) {
+                    size -= 1.f;
+                    mass -= 5.f;
+                    for (int i = 0; i < particles.size(); i++) {
+                        std::cout << particles.at(i)->velocity->x << " : " << particles.at(i)->velocity->y << std::endl;
+                    }
+                }
+                else if (event.key.code == sf::Keyboard::Delete) {
+                    particles.clear();
+                }
+                else {
+                    for (int i = 0; i < 25; i++) {
+                        sf::Vector2f position(mouseX+30*i, mouseY);
+                        sf::Color color(0, 0, 255);
+                        int grav_type = 1;
+                        sf::Vector2f* velocity = new sf::Vector2f(0, 0);
+
+                        Particle* particle = new Particle(size, position, color, grav_type, mass, velocity);
+                        particles.push_back(particle);
+                    }
+                }
+            }
         }
 
         window.clear();
